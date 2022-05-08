9a72b6a6d4d9dc33d74a2829afdf14ce3f4e7237
Mustafa <mmermmus123@gmail.com>
2022-05-08 06:47:53 +0300

    stock page enchanced and some subprograms coded -widget-

--- ManagerSide/contentWidget.cs
+++ ManagerSide/contentWidget.cs
@@ -11,11 +11,157 @@ using RoundBorderLabel;
 
 namespace PQContentWidget
 {
-    public class ContentWidget : Form
+    public class ContentWidget : Button
     {
-        public ContentWidget()
+        //variables
+        RoundLabel baseLabel = new RoundLabel();
+        Font ItalicFont = new Font("Arial", 6, FontStyle.Italic);
+        PictureBox pictureBox1 = new PictureBox();
+        private string fotoToDisplay;
+        private string price;
+        private string stock;
+        private string productBrandAndName;
+        public ContentWidget(ref string fotoPath, string price, ref string stock, ref string brandAndName, ref string explanation)
         {
+            //attempting parameters to variables.
+            this.fotoToDisplay = fotoPath;
+            this.productBrandAndName = brandAndName;
+            this.price = price;
+            this.stock = stock;
+            this.BackColor = Color.FromArgb(30, 30, 30);
+            widget();
+        }
+
+        //our widget that includes a picture box which keeps picture data of product and a label to keep price and stock data.
+        private void widget()
+        {
+            BackColor = Color.FromArgb(30, 30, 30);
+            //label that keeps stock num and price. 
+            //TODO: label's color resists to changing.
+            baseLabel.Size = new Size(100, 50);
+            baseLabel.BackColor = BackColor;
+            baseLabel.cornerRadius = 20;
+            baseLabel.borderWidth = 1;
+            baseLabel.borderColor = Color.White;
+            baseLabel.BorderStyle = BorderStyle.None;
+            baseLabel.Location = new Point(10, 80);
+            baseLabel.Text = productBrandAndName + "\nStock:" + stock + "\nPrice:" + price;
+            baseLabel.Font = ItalicFont;
+            this.Controls.Add(baseLabel);
+            baseLabel.BringToFront();
+
+            //definin image for picturebox and configuring pictureBox
+            Bitmap MyImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            MyImage = new Bitmap(fotoToDisplay);
+            pictureBox1.Width = 60;
+            pictureBox1.Height = 60;
+            pictureBox1.Location = new Point(28, 10);
+            pictureBox1.Image = (Image)MyImage;
+            this.Controls.Add(pictureBox1);
+            pictureBox1.BringToFront();
+
+            this.Click += delegate (object sender, EventArgs e)
+            {
+                ProductPricingPage pricingPage = new ProductPricingPage(ref price, ref productBrandAndName);
+                pricingPage.Size = new Size(400, 600);
+                pricingPage.ShowDialog();
+            };
+
+        }
+    }
+
+    public class ProductPricingPage : Form
+    {
+        //variables.
+        bool answer;
+        string brandAndName;
+        string oldPrice;
+
+        Label label = new Label();
+
+        Label label1 = new Label();//please enter num warning
+
+        TextBox textBox = new TextBox();
+        Font font = new Font("Arial", 12, FontStyle.Regular);
+
+        RJButton buttonYes = new RJButton();
+        RJButton buttonNo = new RJButton();
+
+
+        public ProductPricingPage(ref string price, ref string productBrandAndName)
+        {
+            this.oldPrice = price;
+            this.brandAndName = productBrandAndName;
+            dialogBuilder();
+        }
+
+        private void dialogBuilder()
+        {
+            //dialog header
+            this.BackColor = Color.FromArgb(30, 30, 30);
+            label.Size = new Size(280, 100);
+            label.Location = new Point(40, 40);
+            label.Text = "do you want to add the " + brandAndName + " to the market place ?";
+            label.Font = font;
+            label.ForeColor = Color.White;
+            label.BackColor = BackColor;
+            Controls.Add(label);
+            label.BringToFront();
+
+            //input area
+            textBox.BackColor = BackColor;
+            textBox.ForeColor = Color.White;
+            textBox.Text = this.oldPrice;
+            textBox.Location = new Point(40, 150);
+            textBox.Size = new Size(100, 80);
+            Controls.Add(textBox);
+            textBox.BringToFront();
+
+            //warning message
+            label1.Size = new Size(280, 100);
+            label1.Location = new Point(40, 210);
+            label1.Text = "-Please enter only numberic inputs.";
+            label1.Font = font;
+            label1.ForeColor = Color.White;
+            label1.BackColor = BackColor;
+            Controls.Add(label1);
+            label.BringToFront();
+
+            //answer yes button
+            buttonYes.Size = new Size(120, 70);
+            buttonYes.Location = new Point(50, 420);
+            buttonYes.BackColor = BackColor;
+            buttonYes.BorderSize = 2;
+            buttonYes.BorderRadius = 15;
+            buttonYes.ForeColor = Color.White;
+            buttonYes.BorderColor = Color.White;
+            buttonYes.Font = new Font("Arial", 12, FontStyle.Italic);
+            buttonYes.Text = "yes";
+            buttonYes.Click += delegate (object s, EventArgs e) { };
+            Controls.Add(buttonYes);
+            buttonYes.BringToFront();
+
+
+            //answer no button
+            buttonNo.Size = new Size(120, 70);
+            buttonNo.Location = new Point(220, 420);
+            buttonNo.BackColor = BackColor;
+            buttonNo.BorderSize = 2;
+            buttonNo.BorderRadius = 15;
+            buttonNo.ForeColor = Color.White;
+            buttonNo.BorderColor = Color.White;
+            buttonNo.Text = "no";
+            buttonNo.Font = new Font("Arial", 12, FontStyle.Italic);
+            buttonNo.Click += delegate (object s, EventArgs e) { };
+            Controls.Add(buttonNo);
+            buttonNo.BringToFront();
 
         }
     }
 }
+
+//TODO: i was coding button yes and no's click function.
+//the input area only accepts number
+//if user press yes, move the file to productForSale folder.
+// think about is product can be seen while in productForSale or isn't.
--- ManagerSide/stocks.cs
+++ ManagerSide/stocks.cs
@@ -4,28 +4,23 @@ using System.Drawing;
 using System.Windows.Forms;
 using CustomControls.RJControls;
 using RoundBorderLabel;
+using PQContentWidget;
 
 namespace Stock
 {
 
-    public class Stocks:Form
+    public class Stocks : Form
     {
-        RoundLabel label = new RoundLabel();
-        RJButton ordersButton = new RJButton();
-
-        Font LargeFont = new Font("Arial", 13, FontStyle.Italic);
+        //variables.
+        FlowLayoutPanel panel = new FlowLayoutPanel();
         RoundLabel Border = new RoundLabel();
         string[] catagories = { "woman", "man", "chıld" };
 
 
 
-        public Stocks(){
-
-
+        public Stocks()
+        {
             baseCreator();
-
-
-
         }
 
         private void baseCreator()
@@ -59,18 +54,83 @@ namespace Stock
                 menuButton.BorderSize = 2;
                 menuButton.Text = catagory;
                 menuButton.Font = ItalicFont;
-                menuButton.Click += delegate (object sender, EventArgs e) {  };
+                menuButton.Click += delegate (object sender, EventArgs e) { contentCreator(catagory); };
                 Controls.Add(menuButton);
                 menuButton.BringToFront();
                 point.Y += 120;
             }
+
+            //flow panel that keeps our widgets.
+
+            panel.Location = new Point(200, 40);
+            panel.Size = new Size(540, 380);
+            panel.BackColor = Color.FromArgb(38, 38, 38);
+            panel.FlowDirection = FlowDirection.LeftToRight;
+
+            panel.AutoScroll = true;
+
+            panel.WrapContents = true;
+            Controls.Add(panel);
+            panel.BringToFront();
         }
 
-        private void contentCreator(){
+        private void contentCreator(string catagory)
+        {
             //here we will code new widget which can store picture up, product name, product stock number and also product current price and we will use call this widget with parameters.
-            
+            string fotoPath;
+            string stockNumber;
+            string price;
+            string brandAndProduct;
+            string currentFileContent;
+            string explanation;
+            int index;
+            int index0;
+
+            System.Collections.Generic.IEnumerable<string> files = Directory.EnumerateFiles(@"datas\allStock\" + catagory.ToUpper() + @"\", "*.txt", SearchOption.TopDirectoryOnly);
+            //panel clears itself because if isn't new generated widgets will have piled up.
+            panel.Controls.Clear();
+
+            foreach (string file in files)
+            {
+                //create only related widgets.-to catagory like CHILD,MAN,WOMAN-
+                if (file.Contains(catagory.ToUpper()))
+                {
+                    //getting brand and product name to variable.
+                    brandAndProduct = file.Substring(16 + catagory.Length, file.IndexOf(catagory.ToUpper(), 16 + catagory.Length) - 16 - catagory.Length) + " ";
+                    brandAndProduct += file.Substring(file.IndexOf(catagory.ToUpper(), 16 + catagory.Length) + catagory.Length, file.Length - 4 - (file.IndexOf(catagory.ToUpper(), 16 + catagory.Length) + catagory.Length));
+                    // brandAndProduct += "\n";
+
+                    //read which file we are dealing.
+                    currentFileContent = File.ReadAllText(file);
 
 
+                    //getting stock number as string.
+                    index = currentFileContent.IndexOf(",");
+                    index0 = currentFileContent.IndexOf(":");
+
+                    stockNumber = currentFileContent.Substring(index0 + 1, index - index0 - 1);
+
+                    //getting price number as string.
+                    index0 = currentFileContent.IndexOf("$");
+                    price = currentFileContent.Substring(index + 1, index0 - index - 1);
+
+                    //getting fotographs path as string.
+                    index0 = currentFileContent.IndexOf("&", 2) + 1;
+                    index = currentFileContent.IndexOf("\n", index0);
+                    fotoPath = currentFileContent.Substring(index0, index - index0);
+
+                    //getting product explanation data.
+                    index0 = currentFileContent.IndexOf("&", index) + 1;
+                    index = currentFileContent.IndexOf("\n", index0);
+                    explanation = currentFileContent.Substring(index0, index - index0);
+
+                    //creating button widged according to parameters that above.
+                    ContentWidget modalWidget = new ContentWidget(ref fotoPath, (Convert.ToInt32(price) / Convert.ToInt32(stockNumber)).ToString(), ref stockNumber, ref brandAndProduct, ref explanation);
+                    modalWidget.Size = new Size(120, 150);
+                    panel.Controls.Add(modalWidget);
+                    modalWidget.BringToFront();
+                }
+            }
         }
     }
 }
